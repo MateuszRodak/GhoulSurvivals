51b9a7b3779e9ccec1e97e223c00f1c90c945a71
mateusz <mateusz.rodak108@gmail.com>
2025-04-22 23:39:49 +0200

    v1.0.3 - initial

--- app/src/app.js
+++ app/src/app.js
@@ -1,16 +1,17 @@
 var app = angular.module('myApp', []);
 
-app.controller('MyController', ['$scope', 'controllerModule', function($scope){
+app.controller('appController', ['$scope', '$rootScope', function($scope, $rootScope){
     $scope.hello = 'Hello World from app js';
     $scope.castle = false;
     $scope.castle2 = true;
     $rootScope.isMenu = true;
-    $scope.isMenu2 = false;
+    $scope.isMenu2 = true;
     $scope.changeMenu = changeMenu;
 
-      /*function changeMenu(){
+      function changeMenu(){
         $rootScope.isMenu = false;
-        }*/
+        $scope.isMenu2 = false;
+        }
 
 
 
--- app/src/startView/startViewController.js
+++ app/src/startView/startViewController.js
@@ -1,14 +1,33 @@
-app.controller('startViewController', ['$scope', function($scope){
+
+app.controller('startViewController', ['$scope', '$rootScope', function($scope){
     $scope.name = 'Hello World from startViewController';
     $scope.castle = false;
     $scope.castle2 = true;
-    $rootScope.isMenu = false;
+    $rootScope.isMenu = true;
     $scope.isMenu2 = false;
     $scope.changeMenu = changeMenu;
-}]
 
-    function changeMenu(){
-    $rootScope.isMenu = false;
-    }
-)
+     function changeMenu(){
+        $rootScope.isMenu = false;
+        }
+
+}])
+
+
+
+/*
+function startViewController($scope){
+    $scope.name = 'Hello World from startViewController';
+    $scope.castle = false;
+    $scope.castle2 = true;
+    $rootScope.isMenu = true;
+    $scope.isMenu2 = false;
+    $scope.changeMenu = changeMenu;
+
+     function changeMenu(){
+        $rootScope.isMenu = false;
+        }
+
+}
+*/
 
